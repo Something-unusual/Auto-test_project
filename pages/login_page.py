2c1978a835d51572bf03c5882218c30b5bbb995f
Boris Zatulovskiy <zatulovskiy@mail.ru>
2022-06-27 17:20:42 +0300

    login_page.py, main_page.py and test_main_page.py were updated

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,8 +9,8 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert "login" in self.url, \
-            f"Expected \"login\" to be substring of \"{self.url}\""
+        assert "login" in self.browser.current_url, \
+            f"Expected \"login\" to be substring of \"{self.browser.current_url}\""
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Log In form is not presented"
