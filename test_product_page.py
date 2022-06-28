2d854aafb319502e1439fbd12116f813924e3e93
Something-unusual <104833955+Something-unusual@users.noreply.github.com>
2022-06-28 15:11:46 +0300

    Update test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -27,6 +27,7 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = product_base_link
         page = ProductPage(browser, link)
@@ -43,6 +44,7 @@ def test_guest_cant_see_success_message(browser):
     page.should_not_be_success_message()
 
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('promo_url', [urls[0], urls[1], urls[2], urls[3], urls[4], urls[5], urls[6],
                                        pytest.param(urls[7], marks=pytest.mark.xfail), urls[8], urls[9]])
 def test_guest_can_add_product_to_basket(browser, promo_url):
@@ -80,6 +82,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -87,6 +90,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = BasketPage(browser, link)
